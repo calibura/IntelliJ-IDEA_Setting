e6ecf2891656d9f15b90727fbdc01c777c6339b3
nakamicai <nakamicai@outlook.com>
2023-11-25 21:24:08 +0800

    IU-2021.1.3 <Nakami@NUC11 Create markdown.xml, IntelliLang.xml, Eclipse.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Interface.java, yiiguxing.translation.xml, Betty.xml, Default.xml, find.xml, keymap.xml, code.style.schemes.xml, Darcula copy.icls, javarebel.xml, Xcode-Dark copy.icls, Default.xml, Default copy.xml, Betty copy.xml, encoding.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, getterTemplates.xml, user.xml, dracula-theme.xml, postfixTemplates.xml, profiler.xml, shared-indexes.xml, terminal.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, javaeeExternalResources.xml, memory.view.xml, colors.scheme.xml, databaseDrivers.xml, templates.xml, Windows Proper Redo.xml, consoleFolding.xml, Windows copy.xml, _@user_Xcode-Dark.icls, laf.xml, Class.java, jdk.table.xml, path.macros.xml, acceptedLanguageLevels.xml, wsl.distributions.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, grazie_global.xml, toStringTemplates.xml, usageView.xml, RecognEyes.icls

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @description: ${description} 
+ * @author: NakamiCai
+ * @date:  ${DATE}
+ **/
+public class ${NAME} {
+}
